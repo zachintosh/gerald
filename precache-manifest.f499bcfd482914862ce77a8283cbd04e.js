6771d428e844bfa40930f07593872efefd7a63e5
Zachary Williams <zlw11063@gmail.com>
2019-06-15 21:54:24 -0600

    Updates

---
+++ precache-manifest.f499bcfd482914862ce77a8283cbd04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cf79a87f053f313b80d36ea4df7712",
+    "url": "/gerald/index.html"
+  },
+  {
+    "revision": "d52f8682c900c0cfa36f",
+    "url": "/gerald/static/css/main.ed1fbedc.chunk.css"
+  },
+  {
+    "revision": "c30300965ad885cd301c",
+    "url": "/gerald/static/js/2.2b9a7a61.chunk.js"
+  },
+  {
+    "revision": "d52f8682c900c0cfa36f",
+    "url": "/gerald/static/js/main.ec2e6f8c.chunk.js"
+  },
+  {
+    "revision": "b038079dd96f0bc92888",
+    "url": "/gerald/static/js/runtime~main.0595ff9d.js"
+  }
+]);
